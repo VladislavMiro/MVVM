40161238155d065f366cc94c7b242aa25c5703ac
Vladislav Miroshnichenko <vladislavmiro@icloud.com>
2021-09-25 19:35:11 +0300

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Vladislav Miroshnichenko on 25.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
